a08502dc1067c59039d08238a4e3ef75787bbe33
Dorisl0707 <74819616+Dorisl0707@users.noreply.github.com>
2021-02-23 13:27:34 +0800

    Update height.js

--- height.js
+++ height.js
@@ -11,7 +11,7 @@ const headers = {
 'X-Session-ID' : `1C859C56-85B3-4DD1-BFF3-2B4E11306CFF`,
 'User-Agent' : `Sky-Main-com.netease.sky/0.7.1.160990 (iPad8,9; iphoneos 14.4.0; zh-Hans)`
 };
-const body = `{"user":"dc7e79ca-07fe-486f-8331-156adc2ea546","body":"33emCbjTNwxqD95KSJFfLrG8cOFmefe9llHbZZv4pxGClcJ/U9z1vMPDkGK4rCzILZFAVJji1A4yp4eK9n2XlVayf2XtD+eWOgNX7jU8pZkYEJch5WA+tG1/3qU="}`;
+const body = `{"user":"1ab7d24c-ef8f-451f-9b64-9d218df7c5b8","body":"33emCbjTNwxqD95KSJFfLrG8cOFmefe9llHbZZv4pxGClcJ/U9z1vMPDkGK4rCzILZFAVJji1A4yp4eK9n2XlVayf2XtD+eWOgNX7jU8pZkYEJch5WA+tG1/3qU="}`;
 
 const myRequest = {
     url: url,
